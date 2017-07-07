64f0c2bfe5fe28478eb470217f14c0c9fb5234e5
dkitchenerable <kitchenerdavid@gmail.com>
2017-07-07 14:51:19 -0500

    fix bug in rotated search, finish test coverage

--- arrays/search_rotated_array.rb
+++ arrays/search_rotated_array.rb
@@ -1,7 +1,7 @@
 # problem: given an array has some amount of rotations, determine if it contains a value
 # time: O(logn)
 # space: O(1)
-# solution: if one half is sorted and val is in range, then normal binary
+# solution: if one half is sorted and val is in range, then normal binary search
 # else find the unsorted side and search there
 def search_rotated(arr, val)
   rec_search(arr, 0, arr.size-1, val)
@@ -9,33 +9,16 @@ end
 
 def rec_search(arr, left, right, val)
   return false if left > right
-  middle = ((left + right) / 2).floor
+  middle = (left + right) / 2
   return true if arr[middle] == val
+  
   if (arr[left] < arr[middle]) && (val >= arr[left]) && (val < arr[middle])
     rec_search(arr, left, middle, val)
   elsif (arr[right] > arr[middle]) && (val <= arr[right]) && (val > arr[middle])
-    rec_search(arr, middle, right, val)
+    rec_search(arr, middle + 1, right, val)
   elsif (arr[left] > arr[middle])
     rec_search(arr, left, middle - 1, val)
   else
     rec_search(arr, middle + 1, right, val)
   end
 end
-
-a   = [1,2,3,4,5]
-a1  = [5,1,2,3,4]
-a2  = [4,5,1,2,3]
-a3  = [3,4,5,1,2]
-a4  = [2,3,4,5,1]
-a5  = [4,4,4,5,5]
-
-p search_rotated(a,1)
-p search_rotated(a1,1)
-p search_rotated(a2,1)
-p search_rotated(a3,1)
-p search_rotated(a4,1)
-p search_rotated(a5,5)
-p search_rotated(a5,4)
-
-# false cases
-p search_rotated(a5,1)
--- tests/arrays/search_rotated_array_test.rb
+++ tests/arrays/search_rotated_array_test.rb
@@ -4,6 +4,7 @@ require_relative '../../arrays/search_rotated_array.rb'
 class TestSearchRotatedArray < MiniTest::Unit::TestCase
   def setup
     @array = [1,2,3,4,5]
+    @one_r = [5,1,2,3,4]
   end
 
   def test_empty
@@ -14,11 +15,28 @@ class TestSearchRotatedArray < MiniTest::Unit::TestCase
     assert_equal(false, search_rotated(@array, 6))
   end
 
+  def test_middle
+    assert_equal(false, search_rotated(@array, 6))
+  end  
+
   def test_zero_rotations_match
-    assert_equal(false, search_rotated(@array, 5))
+    assert(search_rotated(@array, 5))
   end
 
-  def test_rotations_match
+  def test_rotations_match_sorted_side
+    assert(search_rotated(@one_r, 3))
+  end
+
+  def test_rotations_match_unsorted_side
+    assert(search_rotated(@one_r, 1))
+  end
+
+  def test_rotations_match_sorted_side_end
+    assert(search_rotated(@one_r, 4))
+  end
+
+  def test_rotations_match_unsorted_side_end
+    assert(search_rotated(@one_r, 5))
   end
 
 end
