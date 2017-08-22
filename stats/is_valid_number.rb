39f9eb3b6a8b4b975013f1fc83752eeefcb44023
dkitchenerable <kitchenerdavid@gmail.com>
2017-08-22 11:49:17 -0400

    test cover is valid_number, add some fixes

--- stats/is_valid_number.rb
+++ stats/is_valid_number.rb
@@ -5,18 +5,31 @@
   space O(1)
 =end
 def is_valid_number?(num_string)
- current_state = 'START'
+ current_state = "START"
  num_string.chars.each do |num|
-
   current_state = advance_state(num, current_state)
   return false if current_state == "WRONG"
  end
- return true
+ current_state != "START"
 end
 
 def advance_state(ch, state)
   case state
-  when "START","INTEGER"
+  when "START"
+    if (ch >= '1' && ch <= '9')
+      return "INTEGER"
+    elsif(ch == '0')
+      return "LEADING_ZERO"
+    else
+      return "WRONG"
+    end
+  when "LEADING_ZERO"
+    if (ch == '.')
+      return "DECIMAL"
+    else
+      return "WRONG"
+    end
+  when "INTEGER"
     if (ch == '.')
       return "DECIMAL"
     elsif (ch >= '0' && ch <= '9')
@@ -32,5 +45,3 @@ def advance_state(ch, state)
     end
   end
 end
-
-p is_valid_number?("1.1")
--- tests/stats/is_valid_number_test.rb
+++ tests/stats/is_valid_number_test.rb
@@ -3,19 +3,35 @@ require_relative '../../stats/is_valid_number.rb'
 
 class TestIsValidNumber < MiniTest::Unit::TestCase
 
-  def test_empty_array
-    assert_nil(missing_number([]))
+  def test_empty_string
+    assert_equal(false, is_valid_number?(""))
   end
 
-  def test_single_element
-    assert_equal(1, missing_number([2]))
+  def test_valid_non_decimal
+    assert(is_valid_number?("123"))
   end
 
-  def test_multiple_elements
-    assert_equal(4, missing_number([1,2,3]))
+  def test_valid_decimal
+    assert(is_valid_number?("123.124"))
   end
 
-  def test_multiple_elements_out_of_order
-    assert_equal(4, missing_number([2,3,5,1]))
+  def test_leading_decimal
+    assert_equal(false, is_valid_number?(".123"))
   end  
+
+  def test_leading_zero
+    assert_equal(false, is_valid_number?("0123"))
+  end    
+
+  def test_invalid_end
+    assert_equal(false, is_valid_number?("123.124."))
+  end  
+
+  def test_not_a_digit
+    assert_equal(false, is_valid_number?("12F003"))
+  end  
+
+  def test_multiple_decimals
+    assert_equal(false, is_valid_number?("12.12.1"))
+  end
 end
